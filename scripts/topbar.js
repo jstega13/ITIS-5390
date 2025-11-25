f2fc0ba7ef9ebca9a2f085d7633ecb45cd6bacf9
Bpeters0403 <brandon.peters0403@gmail.com>
2025-11-25 14:30:51 -0500

    Update topbar.js

--- scripts/topbar.js
+++ scripts/topbar.js
@@ -1,1 +1,8 @@
+// Inject topbar component
+fetch("/components/topbar.html")
+    .then(r => r.text())
+    .then(html => {
+        document.getElementById("topbar").innerHTML = html;
+    })
+    .catch(err => console.error("Topbar load error:", err));
 
